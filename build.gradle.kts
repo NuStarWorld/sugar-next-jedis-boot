bddaa0caff899eb80a305cc3fa4590b24f4369b6
NuStar <3318029085@qq.com>
2026-02-26 03:06:27 +0800

    chore(build): 移除 Maven Central 发布配置，改为使用 nustar-snapshots 仓库

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,73 +59,10 @@ dependencies {
 publishing {
     repositories {
         project(project)
-    }
-    publications {
-        main {
-            pom {
-                description.set("Coffee(Java) with sugar is sweeter.")
-                url.set("https://github.com/ideal-state/sugar-next-jedis-boot")
-                licenses {
-                    license {
-                        name.set("Apache License 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                scm {
-                    url.set("https://github.com/ideal-state/sugar-next-jedis-boot")
-                    connection.set("scm:git:https://github.com/ideal-state/sugar-next-jedis-boot.git")
-                    developerConnection.set("scm:git:https://github.com/ideal-state/sugar-next-jedis-boot.git")
-                }
-                developers {
-                    developer {
-                        id.set("ideal-state")
-                        name.set("ideal-state")
-                        email.set("support@idealstate.team")
-                    }
-                }
-            }
-        }
-    }
-}
-
-jreleaser {
-    deploy {
         maven {
-            mavenCentral {
-                create("release") {
-                    active.set(Active.RELEASE)
-                    url.set("https://central.sonatype.com/api/v1/publisher")
-                    sign.set(false)
-                    stagingRepository("build/repository")
-                }
-            }
-            nexus2 {
-                create("snapshot") {
-                    active.set(Active.SNAPSHOT)
-                    url.set("https://central.sonatype.com/repository/maven-snapshots")
-                    snapshotUrl.set("https://central.sonatype.com/repository/maven-snapshots")
-                    sign.set(false)
-                    applyMavenCentralRules.set(true)
-                    snapshotSupported.set(true)
-                    closeRepository.set(true)
-                    releaseRepository.set(true)
-                    verifyPom.set(false)
-                    stagingRepository("build/repository")
-                }
-            }
+            name = "nustar-snapshots"
+            url = uri("https://maven.nustar.top/repository/nustar-snapshots/")
+            properties(project).login()
         }
     }
-}
-
-tasks.register("doDeploy") {
-    dependsOn(tasks.named("test"))
-    dependsOn(tasks.named("publishAllPublicationsToProjectRepository"))
-    finalizedBy(tasks.named("jreleaserDeploy"))
-}
-
-tasks.register("deploy") {
-    group = "glass"
-    dependsOn(tasks.named("clean"))
-    dependsOn(tasks.named("spotlessApply"))
-    finalizedBy(tasks.named("doDeploy"))
 }
